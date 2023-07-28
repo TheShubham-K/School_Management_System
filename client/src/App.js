178a2c8b7b23ce2008dbc47cd91d830762e03f6c
TheShubham-K <subham.kumar032@gmail.com>
2023-07-28 12:49:22 +0530

    updated the codebase after long time:

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,9 @@ import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
 // import Home from './components/screens/Home'
 //  import Signin from './components/screens/SignIn'
 import Signin from "./Attendance/screens/Signin"
-// import Profile from './components/screens/Profile'
 import Profile from "./Attendance/screens/Profile"
-// import Signup from './components/screens/Signup2'
-//import SignUp from './Attendance/screens/Signup';
 import SignUp from "./Attendance/screens/Auth/Signup"
 import TeacherSignUp from "./Attendance/screens/Auth/Tecah_regis"
-//import CreatePost from './components/screens/CreatePost';
 import AttenHome from "./Attendance/Home"
 import AdminDashboard from "./Attendance/screens/Admin_dash"
 
@@ -28,7 +24,7 @@ const Routing = ()=>{
     const user = JSON.parse(localStorage.getItem("currentUser"))
     console.log("user",user)
     if(user){
-      // dispatch({type:"USER",payload:user})
+      //dispatch({type:"USER",payload:user})
     }else{
       history.push('/landing')
     }
